71331a155db171f54e54fe7c6f4ce2c7d350de18
Your Name <lcasanova88@gmail.com>
2022-05-19 14:53:24 -0700

    Initial Commit

---
+++ Models/MongoDbSettings.cs
@@ -0,0 +1,8 @@
+namespace FFXI_Tools_Api_dotnet.Models;
+
+public class MongoDbSettings
+{
+  public string ConectionURI { get; set; } = null!;
+  public string DatabaseName { get; set; } = null!;
+  public string CollectionName { get; set; } = null!;
+}
